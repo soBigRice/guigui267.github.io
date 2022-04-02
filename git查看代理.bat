e68ec3f614f25ccb0bf7c69d54f725d88467488d
米大饭 <63463970+guigui267@users.noreply.github.com>
2022-04-02 11:32:38 +0800

    git一键代理

---
+++ git查看代理.bat
@@ -0,0 +1,5 @@
+git config --global --get http.proxy
+git config --global --get https.proxy
+
+
+pause
---
+++ git设置代理.bat
@@ -0,0 +1,4 @@
+git config --global http.proxy 'http://127.0.0.1:10809' 
+git config --global https.proxy 'https://127.0.0.1:10809'
+
+pause
